2d1742f93f3f613e2593babf2e06633bbcafc0dc
rahachu <49639890+rahachu@users.noreply.github.com>
2020-06-22 01:23:27 +0700

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "waktuV2";
+const CACHE_NAME = "waktuV1.1";
 var urlsToCache = [
   "./",
   "./nav.html",
@@ -19,7 +19,16 @@ var urlsToCache = [
   "./images/img4.jpg",
   "./images/img5.jpg",
   "https://fonts.googleapis.com/icon?family=Material+Icons",
-  "https://media.giphy.com/media/WSrJg2RqlMPHrgMpOh/source.gif"
+  "https://media.giphy.com/media/WSrJg2RqlMPHrgMpOh/source.gif",
+  "manifest.json",
+  "images/icons/icon-72x72.png",
+  "images/icons/icon-96x96.png",
+  "images/icons/icon-128x128.png",
+  "images/icons/icon-144x144.png",
+  "images/icons/icon-152x152.png",
+  "images/icons/icon-192x192.png",
+  "images/icons/icon-384x384.png",
+  "images/icons/icon-512x512.png"
 ];
  
 self.addEventListener("install", function(event) {
